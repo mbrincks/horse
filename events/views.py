cebf6e0faed69de8074d320abd2ffbc4a56446ae
Megan Brincks <megan_brincks@hotmail.com>
2011-11-01 21:02:00 -0500

    edited views and urls want to kill self

--- events/views.py
+++ events/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from horseproject.eventsapp.models import Event, School
+from django.shortcuts import render_to_response
+
+def homepage(request):
+    event_list = Event.objects.order_by('date')
+    return render_to_response('homepage.html', {'events': events,})
--- urls.py
+++ urls.py
@@ -1,15 +1,14 @@
-from django.conf.urls.defaults import patterns, include, url
-
+from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'horse.views.home', name='home'),
+    (r'^$', 'horseproject.eventsapp.views.homepage'),
+    (r'^event/$', 'horse.events.views.event_list'),
+
     # url(r'^horse/', include('horse.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
